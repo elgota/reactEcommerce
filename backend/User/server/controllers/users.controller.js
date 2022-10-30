183eec786b55a8a9e21fb317b58a4a5900da3d7f
Juan Pablo <elgota8@gmail.com>
2022-10-30 18:04:35 -0300

    primer commit Juan 3

---
+++ backend/User/server/controllers/users.controller.js
@@ -0,0 +1,73 @@
+import { pool } from "../db.js";
+
+export const getUsers = async (req, res) => {
+    const [result] = await pool.query("SELECT * FROM user ORDER BY registeredAt ASC"
+    );
+
+    res.json(result);
+}
+
+export const getUser = async (req, res) => {
+   const [result] = await pool.query("SELECT * FROM user WHERE id = ?", [
+    req.params.id,
+   ])
+
+   if (result.length === 0) {
+    return res.status(404).json({ message: "Usuario no encontrado"});
+    
+   }
+
+   res.json(result[0]);
+}
+
+export const createUser = async (req, res) => {
+
+    const { nombre, apellido, telefono, email, passwordHash } = req.body;
+    const [result] = await pool.query("INSERT INTO user (nombre, apellido, telefono, email, passwordHash) VALUES (?, ?, ?, ?, ?)", 
+    [
+        nombre,
+        apellido,
+        telefono,
+        email,
+        passwordHash
+    ]);
+    console.log(result)
+    res.json({
+        id: result.insertId,
+        nombre,
+        apellido,
+        telefono,
+        email,
+        passwordHash
+    });
+    
+}
+
+export const updateUser = async (req, res) => {
+    const [result] = await pool.query("UPDATE user SET ? WHERE id = ?", [
+    req.body,
+    req.params.id,
+    ]);
+
+    if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Usuario no encontrado" })
+    
+    }
+
+    return res.sendStatus(204);
+    
+}
+
+export const deleteUser = async (req, res) => {
+    const [result] = await pool.query("DELETE FROM user WHERE id = ?",[
+    req.params.id,
+    ]);
+
+    if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Usuario no encontrado" })
+    
+    }
+
+    return res.sendStatus(204);
+};
+
---
+++ backend/User/server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+import indexRoutes from "./routes/index.routes.js";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(indexRoutes);
+app.use(userRoutes);
+
+app.listen(PORT);
+console.log(`Server is listening on port ${PORT}`);
