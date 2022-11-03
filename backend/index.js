61cad4ce99d542dd70999cccacabdcf538db7e1f
Angelo Huaraca <ahuaracab@gmail.com>
2022-11-03 00:04:57 -0500

    Add complete database

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import { PORT } from "./config.js";
 
 import indexRoutes from "./routes/index.routes.js";
-import productsRoutes from './routes/products.routes.js';
-import usersRoutes from './routes/users.routes.js';
-import ordersRoutes from './routes/orders.routes.js';
+import productsRoutes from "./routes/products.routes.js";
+import usersRoutes from "./routes/users.routes.js";
+import ordersRoutes from "./routes/orders.routes.js";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(indexRoutes);
 app.use(productsRoutes);
