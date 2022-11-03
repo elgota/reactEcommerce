71fc96712b9715fc4f7c39069b1d16ed66add06e
Juan Pablo <elgota8@gmail.com>
2022-11-02 22:35:22 -0300

    segundo commit

---
+++ backend/Order/server/controllers/order.controller.js
@@ -0,0 +1,77 @@
+import { pool } from "../db.js";
+
+
+export const getOrders = async (req, res) => {
+    const [result] = await pool.query("SELECT * FROM `order`"
+    );
+
+    res.json(result);
+}
+
+export const getOrder = async (req, res) => {
+    const [result] = await pool.query("SELECT * FROM `order` WHERE id = ?", [
+     req.params.id,
+    ])
+ 
+    if (result.length === 0) {
+     return res.status(404).json({ message: "Usuario no encontrado"});
+     
+    }
+ 
+    res.json(result[0]);
+ }
+
+ export const createOrder = async (req, res) => {
+    const { nombre, apellido, telefono, email, line1, line2, province, country } = req.body;
+    const [result] = await pool.query('INSERT INTO `order` (nombre, apellido, telefono, email, line1, line2, province, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+    [
+        nombre,
+        apellido,
+        telefono,
+        email,
+        line1,
+        line2,
+        province,
+        country
+    ]);
+    console.log(result)
+    res.json({
+        id: result.insertId,
+        nombre,
+        apellido,
+        telefono,
+        email,
+        line1,
+        line2,
+        province,
+        country
+    });
+ }
+
+ export const updateOrder = async (req, res) => {
+    const [result] = await pool.query("UPDATE `order` SET ? WHERE id = ?", [
+    req.body,
+    req.params.id,
+    ]);
+
+    if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Orden no encontrada" })
+    
+    }
+
+    return res.sendStatus(204);
+    
+}
+
+export const deleteOrder = async (req, res) => {
+    const [result] = await pool.query("DELETE FROM `order` WHERE id = ?",[
+    req.params.id,
+    ]);
+
+    if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Orden no encontrada" })
+    
+    }
+
+    return res.sendStatus(204);
+};
---
+++ backend/Order/server/routes/order.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import {
+    getOrders,
+    getOrder,
+    createOrder,
+    updateOrder,
+    deleteOrder
+} from "../controllers/order.controller.js"
+
+const router = Router();
+
+router.get("/api/orders", getOrders);
+router.get("/api/orders/:id", getOrder);
+router.get("/api/orders", createOrder);
+router.get("/api/orders/:id", updateOrder);
+router.get("/api/orders/:id", deleteOrder);
+
+export default router;
