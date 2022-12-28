ff720a7f94c15a0454773f3ff4eb208009047007
Juan Gottardini <elgota8@gmail.com>
2022-12-27 21:06:10 -0300

    Funcionando post product

--- frontend/src/api/product.api.js
+++ frontend/src/api/product.api.js
@@ -1,9 +1,8 @@
 
-import { axios } from 'axios';
+import axios from 'axios';
 
-const createProductRequest = async (product) => {
+export const createProductRequest = async (product) => {
     await axios.post("http://localhost:4000/api/products", product)
 
 }
 
-export default createProductRequest;
--- frontend/src/components/NuevoProducto/NuevoProducto.jsx
+++ frontend/src/components/NuevoProducto/NuevoProducto.jsx
@@ -13,10 +13,16 @@ function NuevoProducto() {
           </nav>
           <Formik
             initialValues= {{
+                userId: "",
                 title: "",
+                slug: "",
                 summary: "",
+                type: "",
+                sku: "",
                 price: "",
+                discount: "",
                 quantity: "",
+                shop: ""
 
 
             }}
@@ -26,29 +32,53 @@ function NuevoProducto() {
                     console.log(values);
                     const response = await createProductRequest(values)
                 } catch (error) {
-                    console.log(error)
+                    console.error(error)
                 }
                 
             }}
           >
         {({handleChange, handleSubmit}) => (
            <div className="productoForm">
-           <Form className="formInputs" onSubmit={handleSubmit}> 
+           <Form className="formInputs" onSubmit={handleSubmit}>
+                <div className="mb-3">
+                   <label className="form-label">Id del usuario</label>
+                   <input type="number" name="userId" className="form-control" onChange={handleChange} />
+               </div> 
                <div className="mb-3">
                    <label className="form-label">Nombre del producto</label>
                    <input type="text" name="title" className="form-control" onChange={handleChange} />
+               </div>
+               <div className="mb-3">
+                   <label className="form-label">Slug</label>
+                   <input type="text" name="slug" className="form-control" onChange={handleChange} />
                </div>
                <div className="mb-3">
                    <label className="form-label">Resumen</label>
                    <textarea type="text" name="summary" rows="3" className="form-control" onChange={handleChange}  />
                </div>
                <div className="mb-3">
+                   <label className="form-label">Tipo</label>
+                   <input type="text" name="type" className="form-control" onChange={handleChange}  />
+               </div>
+               <div className="mb-3">
+                   <label className="form-label">Sku</label>
+                   <input type="text" name="sku" className="form-control" onChange={handleChange}  />
+               </div>
+               <div className="mb-3">
                    <label className="form-label">Precio</label>
                    <input type="text" name="price" className="form-control" onChange={handleChange}  />
                </div>
                <div className="mb-3">
+                   <label className="form-label">Descuento</label>
+                   <input type="text" name="discount" className="form-control" onChange={handleChange}  />
+               </div>
+               <div className="mb-3">
                    <label className="form-label">Unidades disponibles</label>
                    <input type="number" name="quantity" className="form-control" onChange={handleChange}  />
+               </div>
+               <div className="mb-3">
+                   <label className="form-label">Disponible</label>
+                   <input type="text" name="shop" className="form-control" onChange={handleChange}  />
                </div>
 
                <button type="submit" className="btn btn-primary">Cargar</button>
