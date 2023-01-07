a1395217c5942571c814108d49f8f8ecdf5e2243
Santiago Tonin <santiagotonin@hotmail.com>
2023-01-07 18:11:07 -0300

    maquetado agregar producto beta

--- frontend/src/components/addProduct/AddProduct.jsx
+++ frontend/src/components/addProduct/AddProduct.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import css from "./AddProduct.module.css";
+
+import imgLogo from "../../assets/logoVivero.png"
+import fondo from "../../assets/fondo-vivero.jpg"
 
 const AddProduct = () => {
 
   const [preImg, setpreImg] = useState([]);
+  const[text ,setText] = useState("texto 1");
 
   const changeInput = (e) => {
     let indexImg;
@@ -15,13 +19,14 @@ const AddProduct = () => {
       indexImg = 0;
     }
 
-    let newImgToState = readmultifile(e, indexImg);
-    let newImgState = [...preImg, ...newImgToState];
-    setpreImg(newImgState);
+
+    let newImgsToState = readmultifile(e, indexImg);
+    let newImgsState = [...preImg, ...newImgsToState];
+    setpreImg(newImgsState);
   }
 
   function readmultifile(e, indexImg) {
-    const files = e.currentTarget.file;
+    const files = e.currentTarget.files;
 
     const arrayImages = [];
 
@@ -40,72 +45,100 @@ const AddProduct = () => {
     return arrayImages;
   }
 
-  function deleteImg(indice){
-    const newImgs = preImg.filter(function (element){
+  function deleteImg(indice) {
+    const newImgs = preImg.filter(function (element) {
       return element.index !== indice;
     });
     console.log(newImgs)
     setpreImg(newImgs)
   }
 
+  const texts = ["texto 1","texto 2","texto 3"];
+  let index = 0;
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setText(texts[index]);
+      index =(index +1)% texts.length;
+    },5000);
+    return ()=>clearInterval(interval);
+  },[])
+
 
 
   return (
     <div className={css.container_form}>
-      <div className={css.container_info}></div>
+      <div className={css.container_info}>
+        <img src={fondo} alt="vivero" className={css.imagenFondo} />
+        <img src={imgLogo} alt="logo" className={css.logo} />
+        <p>{text}</p>
+      </div>
       <div className={css.form_product}>
         <form action="">
 
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Nombre Del Producto</label>
           </div>
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Title</label>
           </div>
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Slug</label>
           </div>
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Summary</label>
           </div>
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Type</label>
           </div>
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Sku</label>
           </div>
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Precio</label>
           </div>
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Descuento</label>
           </div>
           <div className={css.input_group}>
-            <input required="" type="text" name="text" autocomplete="off" className={css.inputs} />
+            <input required="" type="text" name="text"  className={css.inputs} />
             <label className={css.user_label}>Unidades disponibles</label>
           </div>
           <label className={css.btnLabel}>
             <div className={css.btnSelector}>
-              <span>Seleccionar archivos </span>
-              <input hidden type="file" multiple ></input>
+              <span>Agregar Imagen </span>
+              <input hidden type="file" multiple onChange={changeInput}></input>
             </div>
           </label>
-          <div className={css.filaImg}>
-            <div className={css.caja}>
-              <button className={css.btnDelete}>X</button>
-            </div>
+         {preImg.length > 0 && <div className={css.filaImg}>
+            {             
+              preImg.map((imagen) => {
+                return <div className={css.caja} key={imagen.index}>
+                    <button  className={css.btnDelete} onClick={deleteImg.bind(this, imagen.index)}>X</button>
+                    <img
+                      alt="algo"
+                      src={imagen.url}
+                      data-toggle="modal"
+                      data-target="#ModalPreViewImg"
+                      className={css.img_responsive}
+                    ></img>
+                  </div>
+              })
+            }
 
-          </div>
+          </div>}
+          <div className={css.btnUnder}>
           <button type='submit' className={css.btnSend}>Aceptar</button>
           <button type='submit' className={css.btnSend}>Limpiar</button>
+          </div>
         </form>
       </div>
     </div>
