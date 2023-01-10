4c198026e495b3667486c59e73e366a60ed10407
Angelo Huaraca <ahuaracab@gmail.com>
2023-01-10 15:58:19 -0500

    resolve conflict product-crud and dev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import 'boxicons';
+import "boxicons";
 import NavBar from "./components/NavBar/NavBar";
 import NuevoProducto from "./components/NuevoProducto/NuevoProducto";
 import { Routes, Route } from "react-router-dom";
 import { NuevaImagen } from "./components/nuevaImagen/NuevaImagen";
 import VerProductos from "./components/VerProductos/VerProductos";
-
+import ImagesUploadPage from "./pages/ImagesUploadPage";
+import ImagesByProductIdPage from "./pages/ImagesByProductIdPage";
 
 function App() {
   return (
@@ -14,12 +15,12 @@ function App() {
       <NavBar />
 
       <Routes>
-
         <Route path="/" element={<NuevoProducto />}></Route>
         <Route path="/nuevaImagen" element={<NuevaImagen />}></Route>
-        <Route path="/productos" element={<VerProductos/>}></Route>
+        <Route path="/productos" element={<VerProductos />}></Route>
+        <Route path="/images-upload" element={<ImagesUploadPage />} />
+        <Route path="/images" element={<ImagesByProductIdPage />} />
       </Routes>
-
     </div>
   );
 }
