bedc2c5c831799672cec6704a38169d0eecff0ba
Angelo Huaraca <ahuaracab@gmail.com>
2023-01-09 19:16:38 -0500

    Change config vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import ImagesUploadPage from "./pages/ImagesUploadPage";
 import ImagesByProductIdPage from "./pages/ImagesByProductIdPage";
+import HelloWorld from "./pages/HelloWorld";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<HelloWorld />} />
       <Route path="/images-upload" element={<ImagesUploadPage />} />
       <Route path="/images" element={<ImagesByProductIdPage />} />
     </Routes>
