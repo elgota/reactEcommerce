0cef6b48b9902d3731774dc5e58800fb7045b462
Angelo Huaraca <ahuaracab@gmail.com>
2023-01-15 00:58:12 -0500

    Fix merge conflicts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,3 +12,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+reportWebVitals();
