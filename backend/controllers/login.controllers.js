a0f106a8d98cd5a2dcae7462edf832dbd3fd3b0d
Angelo Huaraca <ahuaracab@gmail.com>
2023-01-18 00:20:49 -0500

    add controller and route to login

---
+++ backend/controllers/login.controllers.js
@@ -0,0 +1,24 @@
+import { pool } from "../db.js";
+import bcrypt from "bcrypt";
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const [result] = await pool.query("SELECT * FROM users WHERE email = ?", [
+    email,
+  ]);
+
+  if (result.affectedRows === 0) {
+    return res.status(404).json({ message: "Usuario o contraseña inválida" });
+  }
+  const hash = result[0].passwordHash;
+  const match = await bcrypt.compare(password, hash);
+  if (match) {
+    return res.sendStatus(200).json({
+      message: "Inicio de sesión exitoso",
+    });
+  } else {
+    return res.sendStatus(400).json({
+      message: "Usuario o contraseña inválida",
+    });
+  }
+};
