5109b288c510a1ad26ab8d61bdafe6af2e2673c4
Angelo Huaraca <ahuaracab@gmail.com>
2023-01-25 17:23:26 -0500

    Add cart router and controller

--- backend/controllers/cart.item.controllers.js
+++ backend/controllers/cart.item.controllers.js
@@ -46,6 +46,26 @@ export const getCartItems = async (req, res) => {
   res.json(result);
 };
 
+export const getCartItemsByUserId = async (req, res) => {
+  var [result] = await pool.query(
+    "SELECT * FROM `cart_item` ci INNER JOIN `cart` c WHERE ci.idCart = c.id AND c.idUser = ?",
+    [req.query.productId]
+  );
+
+  if (result.length === 0) {
+    return res.status(404).json({ message: "Imagenes no encontradas" });
+  }
+
+  if (req.query.productId === "null") {
+    res.json(imagenDir);
+  } else {
+    const imageDirByProductId = imagenDir.filter((image) =>
+      image.startsWith(req.query.productId + "-")
+    );
+    res.json(imageDirByProductId);
+  }
+};
+
 export const getCartItem = async (req, res) => {
   const [result] = await pool.query("SELECT * FROM `cart_item` WHERE id = ?", [
     req.params.id,
