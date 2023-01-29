59300a78a1e26f8344292319dabb4dd859b9a85a
Angelo Huaraca <ahuaracab@gmail.com>
2023-01-29 01:16:06 -0500

    Update Product.jsx

--- frontend/src/components/products/Products.jsx
+++ frontend/src/components/products/Products.jsx
@@ -1,20 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import css from "./Products.module.css";
 import Plane from "../../assets/plane.png";
-import { productsInitialState } from "../reducers/shoppingCart_reducer";
 import { Link } from "react-router-dom";
 import { useRef } from "react";
-import { CART } from "./../../config/routes/paths.js";
-
-// import axios from 'axios';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { getCustomProductRequest } from "../../api/product.api";
 
 function Products() {
-  // const getCustomProductRequest = async () =>
-  // await axios.get("http://localhost:4000/api/products/custom");
-
   const boxSHDescProd = useRef(null);
   // let nameBoxSHDescProd = '';
   const [iDBoxSHDescProd, setIDBoxSHDescProd] = useState("");
@@ -24,26 +18,41 @@ function Products() {
   const [typeBoxSHDescProd, setTypeBoxSHDescProd] = useState("");
   const [priceBoxSHDescProd, setPriceBoxSHDescProd] = useState("");
 
-  // console.log(getCustomProductRequest());
+  const [MenuProducts, setMenuProducts] = useState([]);
+  const [AllProducts, setAllProducts] = useState([]);
 
-  const [MenuProducts, setMenuProducts] = useState(
-    productsInitialState.products
-  );
+  useEffect(() => {
+    async function loadImages() {
+      const res = await getCustomProductRequest();
+      setMenuProducts(res.data);
+      setAllProducts(res.data);
+    }
 
-  const filter = (type) => {
+    loadImages();
+  }, []);
+
+  const filter = (price) => {
     setMenuProducts(
-      productsInitialState.products.filter((product) => product.type === type)
+      // eslint-disable-next-line
+      AllProducts.filter((product) => {
+        if (price === "lessThan50") {
+          return product.price < 50.0;
+        } else if (price === "between50and100") {
+          return product.price >= 50.0 && product.price <= 100.0;
+        } else if (price === "moreThan100") {
+          return product.price > 100.0;
+        }
+      })
     );
   };
 
   function onBtnShowDesc(refBox, product) {
-    console.log(product);
     refBox.style.display = "block";
     setIDBoxSHDescProd(product.id);
-    setNameBoxSHDescProd(product.name);
-    setImgBoxSHDescProd(product.img);
-    setDetailBoxSHDescProd(product.detail);
-    setTypeBoxSHDescProd(product.type);
+    setNameBoxSHDescProd(product.title);
+    setImgBoxSHDescProd(product.data);
+    setDetailBoxSHDescProd(product.summary);
+    setTypeBoxSHDescProd(product.content);
     setPriceBoxSHDescProd(product.price);
     console.log(nameBoxSHDescProd);
   }
@@ -58,76 +67,78 @@ function Products() {
         <h1>Our Featured products</h1>
         <div className={css.products}>
           <ul className={css.menu}>
-            <li onClick={() => setMenuProducts(productsInitialState.products)}>
-              ALL
-            </li>
-            <li onClick={() => filter("skin care")}>Skin Care</li>
-            <li onClick={() => filter("conditioner")}>Conditioners</li>
-            <li onClick={() => filter("foundation")}>Foundations</li>
+            <li onClick={() => setMenuProducts(AllProducts)}>ALL</li>
+            <li onClick={() => filter("lessThan50")}>Hasta $50</li>
+            <li onClick={() => filter("between50and100")}>$50 a $100</li>
+            <li onClick={() => filter("moreThan100")}>Mas de $100</li>
           </ul>
           <div className={css.list}>
-            {MenuProducts.map((product, i) => (
-              <div className={css.product} key={i}>
-                <div className="left-s">
-                  <div className="name">
-                    <span>{product.name}</span>
-                    <span>{product.detail}</span>
+            {MenuProducts.map((product, i) => {
+              return (
+                <div className={css.product} key={i}>
+                  <div className="left-s">
+                    <div className="name">
+                      <span>{product.title}</span>
+                      <span>{product.summary}</span>
+                    </div>
+                    <span>{product.price}$</span>
+                    <Link to={`{CART}/${product.id}`}>
+                      <span>Añadir al carrito</span>
+                    </Link>
+                    <div
+                      className={css.btnDescPro}
+                      onClick={() =>
+                        onBtnShowDesc(boxSHDescProd.current, product)
+                      }
+                    >
+                      Ver descripción
+                    </div>
                   </div>
-                  <span>{product.price}$</span>
-                  <Link to={`/${CART}/${product.id}`}>
-                    <span>Añadir al carrito</span>
-                  </Link>
-                  <div
-                    className={css.btnDescPro}
-                    onClick={() =>
-                      onBtnShowDesc(boxSHDescProd.current, product)
-                    }
-                  >
-                    Ver descripción
-                  </div>
+                  <img src={product.data} alt="no img" className="img-p"></img>
                 </div>
-                <img src={product.img} alt="" className="img-p"></img>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </div>
       </div>
       <div
-        className={css.divSHDescProd}
+        className={css.mainDivSHDescProd}
         ref={boxSHDescProd}
         style={{ display: "none" }}
       >
-        <div className={css.barTopDivSH}>
-          <FontAwesomeIcon
-            icon={faTimes}
-            onClick={() => onBtnHideDesc(boxSHDescProd.current)}
-          />
-        </div>
-        <div className={css.contentDivSH}>
-          <div
-            className={css.imgSHDescProd}
-            style={{ backgroundImage: `url(${imgBoxSHDescProd})` }}
-          ></div>
-          <div>
-            <p>
-              Nombre producto: <span>{nameBoxSHDescProd}</span>
-            </p>
-            <p>
-              Detalles del producto: <span>{detailBoxSHDescProd}</span>
-            </p>
-            <p>
-              Tipo de producto: <span>{typeBoxSHDescProd}</span>
-            </p>
-            <p>
-              Precio producto: $<span>{priceBoxSHDescProd}</span>
-            </p>
-            <div className={css.divContLink}>
-              <Link
-                to={`/${CART}/${iDBoxSHDescProd}`}
-                className={css.linkCarSHDescProd}
-              >
-                <span>Añadir al carrito</span>
-              </Link>
+        <div className={css.divSHDescProd}>
+          <div className={css.barTopDivSH}>
+            <FontAwesomeIcon
+              icon={faTimes}
+              onClick={() => onBtnHideDesc(boxSHDescProd.current)}
+            />
+          </div>
+          <div className={css.contentDivSH}>
+            <div
+              className={css.imgSHDescProd}
+              style={{ backgroundImage: `url(${imgBoxSHDescProd})` }}
+            ></div>
+            <div>
+              <p>
+                Nombre producto: <span>{nameBoxSHDescProd}</span>
+              </p>
+              <p>
+                Summary: <span>{detailBoxSHDescProd}</span>
+              </p>
+              <p>
+                Precio producto: $<span>{priceBoxSHDescProd}</span>
+              </p>
+              <p>
+                Descripcion detallada: <span>{typeBoxSHDescProd}</span>
+              </p>
+              <div className={css.divContLink}>
+                <Link
+                  to={`/lista-carrito/${iDBoxSHDescProd}`}
+                  className={css.linkCarSHDescProd}
+                >
+                  <span>Añadir al carrito</span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
